a7c5c3b64557ed15f4376e99b11c76089609e322
EashcodeX <eash@example.com>
2026-04-26 11:43:46 +0530

    fix: final cors configuration and imports

--- Autonoma_Backend/src/main/java/com/autonoma/erp/AutonomaBackendApplication.java
+++ Autonoma_Backend/src/main/java/com/autonoma/erp/AutonomaBackendApplication.java
@@ -1,5 +1,7 @@
 package com.autonoma.erp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
