c1f07f39d5ea4f3b1c596bd58e5d979b34c18be1
EashcodeX <eash@example.com>
2026-05-18 12:46:45 +0530

    build(db): completely remove deprecated db/migration directory and fully transition to dbscripts

--- autonoma-backend/src/main/resources/db/migration/V10.0__Professional_Department_Codes.sql
+++
@@ -1,8 +0,0 @@
--- Alter Department Number to String to support professional codes like DEPT-001
--- Drop the default constraint first
-DECLARE @ConstraintName nvarchar(200)
-SELECT @ConstraintName = Name FROM sys.default_constraints WHERE parent_object_id = OBJECT_ID('hrm_department_master') AND parent_column_id = (SELECT column_id FROM sys.columns WHERE parent_object_id = OBJECT_ID('hrm_department_master') AND name = 'dept_no')
-IF @ConstraintName IS NOT NULL EXEC('ALTER TABLE hrm_department_master DROP CONSTRAINT ' + @ConstraintName)
-
-ALTER TABLE hrm_department_master ALTER COLUMN dept_no NVARCHAR(50);
-GO
--- autonoma-backend/src/main/resources/db/migration/V6.9__Expand_Customer_FileUpload__TIS.sql
+++
@@ -1,5 +0,0 @@
--- V3.6 EXPAND FILE UPLOAD COLUMN LENGTH
--- Increases the length of file_upload in SM_CUSTOMER_MASTER and SM_CONTACT_MASTER to support multiple attachments.
-
-ALTER TABLE [dbo].[SM_CUSTOMER_MASTER] ALTER COLUMN [file_upload] NVARCHAR(2000);
-ALTER TABLE [dbo].[SM_CONTACT_MASTER] ALTER COLUMN [file_upload] NVARCHAR(2000);
