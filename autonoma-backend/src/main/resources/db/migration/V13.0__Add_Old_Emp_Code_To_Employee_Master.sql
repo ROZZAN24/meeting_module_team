5c8a75e3b15db09f7a4e3c52507ea61399955d58
Pravin <4developernt@gmail.com>
2026-05-18 11:34:27 +0530

    Removed Db\Migration

--- autonoma-backend/src/main/resources/db/migration/V13.0__Add_Old_Emp_Code_To_Employee_Master.sql
+++
@@ -1,8 +0,0 @@
--- V13.0__Add_Old_Emp_Code_To_Employee_Master.sql
--- Add legacy employee code tracking to the master record
-
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'hrm_employee_master' AND COLUMN_NAME = 'old_emp_code')
-BEGIN
-    ALTER TABLE hrm_employee_master ADD old_emp_code NVARCHAR(50);
-END
-GO
--- autonoma-backend/src/main/resources/db/migration/V17.0__Expand_Prefix_Length.sql
+++
@@ -1,5 +0,0 @@
--- Expand prefix columns to 20 characters
-ALTER TABLE ad_prefix_credentials ALTER COLUMN sales_order_prefix VARCHAR(20);
-ALTER TABLE ad_prefix_credentials ALTER COLUMN mat_po_prefix VARCHAR(20);
-ALTER TABLE ad_prefix_credentials ALTER COLUMN gate_entry_prefix VARCHAR(20);
-ALTER TABLE ad_prefix_credentials ALTER COLUMN invoice_prefix VARCHAR(20);
